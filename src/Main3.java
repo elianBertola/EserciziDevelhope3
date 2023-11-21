f779021f53643c3828bfbccbe0283aca8ddbafc9
elianBertola <150374006+elianBertola@users.noreply.github.com>
2023-11-21 22:00:53 +0100

    Add files via upload

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        Auto car1 = new Auto(4.7, "AA000AA", "Lamborghini", "Diablo");
+        Auto car2 = new Auto(7.9, "AA000AB", "Bugatti", "Veyron");
+
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+}
